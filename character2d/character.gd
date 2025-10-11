c413d628349f946f775a03fd9f844e62f61f36a5
TraighScott <traigh.scott@gmail.com>
2025-10-11 19:25:55 -0400

    Setup player character animations:

--- character2d/character.gd
+++ character2d/character.gd
@@ -1,9 +1,24 @@
 extends CharacterBody2D
 
+@onready var _animated_sprite := $AnimatedSprite2D
 
 const SPEED = 150.0
 
 func _physics_process(_delta: float) -> void:
+	#Handles animations
+	if not Input.get_vector('move_down', 'move_left', 'move_right', 'move_up'):
+		_animated_sprite.play("idle")
+	if Input.is_action_just_pressed("move_down"):
+		_animated_sprite.play("walk_up")
+	elif Input.is_action_just_pressed("move_up"):
+		_animated_sprite.play("walk_down")
+	elif Input.is_action_just_pressed("move_right"):
+		$AnimatedSprite2D.flip_h = false
+		_animated_sprite.play("walk_side")
+	elif Input.is_action_just_pressed("move_left"):
+		$AnimatedSprite2D.flip_h = true
+		_animated_sprite.play("walk_side")
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_vector("move_left", "move_right", "move_down", 'move_up')
